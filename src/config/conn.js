ce322a0bb90b4633aff98e24e8b5781cb0f35849
Gguife <gguife747@gmail.com>
2024-09-06 21:13:23 -0300

    feat: Create connection with DB

---
+++ src/config/conn.js
@@ -0,0 +1,14 @@
+import { Sequelize } from "sequelize";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+
+const {DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT} = process.env;
+const sequelizeConn = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST, 
+  dialect: 'mysql',
+  port: DB_PORT
+});
+
+export default sequelizeConn;
