06bf7e35707ba2ac12c6bddbc6dbae7a6af898b5
Gguife <gguife747@gmail.com>
2024-09-28 20:30:17 -0300

    feat: Server project conclude

--- src/server.js
+++ src/server.js
@@ -1,10 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
+import sequelizeConn from "./config/conn.js";
+import urlRoutes from "./controller/url.js";
+import clickRoutes from "./controller/click.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.listen(8080, () => {
-  console.log("Server running on port 8080");
+app.get("/api", (_req, res) => res.json({ message: "© 2024 - gguife shortened links!" }));
+
+app.use('/', urlRoutes);
+app.use('/click', [clickRoutes]);
+
+sequelizeConn.authenticate().then(() => {
+  console.log(`Database is connected: ${process.env.DB_NAME}`);
+}).catch(error => console.error('Unable to connect to the database:', error));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
 })
