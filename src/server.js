4a2f751ed5274bcc2ce8f2dbbe6c2313093bee2f
Gguife <gguife747@gmail.com>
2024-09-28 21:17:13 -0300

    feat: Add cors configuration

--- src/server.js
+++ src/server.js
@@ -3,11 +3,18 @@ import dotenv from "dotenv";
 import sequelizeConn from "./config/conn.js";
 import urlRoutes from "./controller/url.js";
 import clickRoutes from "./controller/click.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://127.0.0.1:5500',
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+}));
 
 app.get("/api", (_req, res) => res.json({ message: "© 2024 - gguife shortened links!" }));
 
